39dd8f7cf60b6b5e128e41636472e45d3d8a47ba
Levinh <23644831.vinh@student.iuh.edu.vn>
2025-09-02 16:30:24 +0700

    first commit

---
+++ scripts/pipeline.sh
@@ -0,0 +1,25 @@
+nvidia-smi
+source ~/.bashrc
+
+while true; do nvidia-smi > gpu_status.txt; sleep 5; done &
+
+module load shared conda
+. $CONDAINIT
+conda activate sem_deep
+
+conda info --envs
+echo "Conda Environment: $CONDA_DEFAULT_ENV"
+
+export PYTHONPATH="SemViQA:$PYTHONPATH"
+echo "Starting the inference process..."
+
+python3 semviqa/pipeline \
+    --data_path "data/test.json" \
+    --output_path "output.json" \
+    --model_evidence_QA "SemViQA/qatc-infoxlm-viwikifc" \
+    --model_bc "SemViQA/bc-infoxlm-viwikifc" \
+    --model_tc "SemViQA/tc-infoxlm-viwikifc" \
+    --thres_evidence 0.5\
+    
+ 
+echo "Inference process completed!"
---
+++ scripts/qatc.sh
@@ -0,0 +1,44 @@
+nvidia-smi
+source ~/.bashrc
+
+while true; do nvidia-smi > gpu_status.txt; sleep 5; done &
+
+module load shared conda
+. $CONDAINIT
+conda activate sem_deep
+
+conda info --envs
+echo "Conda Environment: $CONDA_DEFAULT_ENV"
+
+export PYTHONPATH="SemViQA:$PYTHONPATH"
+echo "Running training script..."
+
+BS=36
+# If you want to fine-tune a pre-trained model, run the following script.
+python3 -m semviqa.ser.main \
+    --name "isedsc" \
+    --model_name "SemViQA/qatc-infoxlm-viwikifc" \
+    --output_dir "./output" \
+    --train_batch_size $BS \
+    --num_train_epochs 16 \
+    --learning_rate 2e-6 \
+    --train_data  "train.csv" \
+    --eval_data  "test.csv"\
+    --patience 5 \
+    --is_pretrained 1\
+
+# If you want to fine-tune an untrained model, run the following script.
+# model_name = "microsoft/infoxlm-large", "microsoft/infoxlm-base" or "nguyenvulebinh/vi-mrc-large", "nguyenvulebinh/vi-mrc-base"
+python3 -m semviqa.ser.main \
+    --name "isedsc" \
+    --model_name "microsoft/infoxlm-large" \
+    --output_dir "./output" \
+    --train_batch_size $BS \
+    --num_train_epochs 16 \
+    --learning_rate 2e-6 \
+    --train_data  "train.csv" \
+    --eval_data  "test.csv"\
+    --patience 5 \
+    --is_pretrained 0\
+
+echo "Training completed for model"
